1d3dc33eee46e057ff1e666d563113c72f052833
Eiji971 <bosc_emery@outlook.com>
2023-09-03 21:43:33 -0400

    comment line suppressed

--- backend-ext/server.js
+++ backend-ext/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3001; // You can choose any available port
+const port = 3001; 
 
 // Create a MySQL database connection
 const db = mysql.createConnection({
