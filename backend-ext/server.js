8afaa0ea4be287ed980c76d09f55b23b60993b91
Eiji971 <bosc_emery@outlook.com>
2023-09-08 17:23:33 -0400

    add cors for cross origin request

--- backend-ext/server.js
+++ backend-ext/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 
 const app = express();
 const port = 3001; 
+
+
+app.use(cors());
 
 // Create a MySQL database connection
 const db = mysql.createConnection({
